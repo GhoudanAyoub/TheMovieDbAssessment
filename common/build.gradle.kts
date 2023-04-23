ffb8eb50d561b18f3dbf3ad3909af4f7f18fdd4c
ayoub <ayoubghoudanos@gmail.com>
2023-04-23 18:14:22 +0100

    Implementation: local models layer to separate between remote/local classes
    Signed-off-by: ayoub <ayoubghoudanos@gmail.com>

---
+++ common/build.gradle.kts
@@ -0,0 +1,38 @@
+import ghoudan.ayoub.movieBest.Deps
+
+plugins {
+    id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+android {
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+dependencies {
+    implementation(Deps.Androidx.CoreKtx.coreKtx)
+    implementation(Deps.Androidx.AppCompat.appCompat)
+    implementation(Deps.Google.Material.material)
+
+    implementation(Deps.Hilt.hilt)
+    kapt(Deps.Hilt.hilt_compiler)
+
+    implementation(Deps.Networking.logging_interceptor)
+    implementation(Deps.Networking.ok2Curl)
+
+    implementation(Deps.Serialization.retrofitKotlinxSerializationConverter)
+    implementation(Deps.Serialization.kotlinxSerialization)
+
+    implementation(Deps.Coroutines.coroutines)
+
+    implementation(Deps.Timber.timber)
+
+}
