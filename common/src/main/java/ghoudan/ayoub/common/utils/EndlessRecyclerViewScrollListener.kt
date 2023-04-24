4e8d6e82213db78fe5cebb03bb8a9367b7dc43ee
ayoub <ayoubghoudanos@gmail.com>
2023-04-24 02:24:13 +0100

    Implementation: ConnectivityModule to listen on connection availability + adapt code to this change
    Signed-off-by: ayoub <ayoubghoudanos@gmail.com>

--- common/src/main/java/ghoudan/ayoub/common/utils/EndlessRecyclerViewScrollListener.kt
+++ common/src/main/java/ghoudan/ayoub/common/utils/EndlessRecyclerViewScrollListener.kt
@@ -1,20 +1,17 @@
 package ghoudan.ayoub.common.utils
 
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
-/**
- * improved version of:
- * https://gist.github.com/nesquena/d09dc68ff07e845cc622
- */
 abstract class EndlessRecyclerViewScrollListener(
-    private val layoutManager: GridLayoutManager
+    private val layoutManager: RecyclerView.LayoutManager
 ) : RecyclerView.OnScrollListener() {
-
 
     // The minimum amount of items to have below your current scroll position
     // before loading more.
-    private var visibleThreshold = 2
+    private var visibleThreshold = 5
 
     // The current offset index of data you have loaded
     private var currentPage = 1
@@ -28,9 +25,12 @@ abstract class EndlessRecyclerViewScrollListener(
     // Sets the starting page index
     private var startingPageIndex = 1
 
+    private var itemsPerPage = 20
 
     init {
-        visibleThreshold *= layoutManager.spanCount
+        if (layoutManager is GridLayoutManager) {
+            visibleThreshold *= layoutManager.spanCount
+        }
     }
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
@@ -38,8 +38,22 @@ abstract class EndlessRecyclerViewScrollListener(
         var lastVisibleItemPosition = 0
         val totalItemCount: Int = layoutManager.itemCount
         if (totalItemCount >= visibleThreshold) {
-            lastVisibleItemPosition =
-                layoutManager.findLastCompletelyVisibleItemPosition()
+            when (layoutManager) {
+                is StaggeredGridLayoutManager -> {
+                    val lastVisibleItemPositions =
+                        layoutManager.findLastVisibleItemPositions(null)
+                    // get maximum element within the list
+                    lastVisibleItemPosition = getLastVisibleItem(lastVisibleItemPositions)
+                }
+                is GridLayoutManager -> {
+                    lastVisibleItemPosition =
+                        layoutManager.findLastVisibleItemPosition()
+                }
+                is LinearLayoutManager -> {
+                    lastVisibleItemPosition =
+                        layoutManager.findLastVisibleItemPosition()
+                }
+            }
 
             // If the total item count is zero and the previous isn't, assume the
             // list is invalidated and should be reset back to initial state
@@ -77,7 +91,8 @@ abstract class EndlessRecyclerViewScrollListener(
                 && isLastItemDisplaying(recyclerView)
             ) {
                 currentPage++
-                onLoadMore(currentPage)
+                itemsPerPage += 20
+                onLoadMore(currentPage, itemsPerPage, recyclerView)
                 loading = true
             }
         }
@@ -87,7 +102,7 @@ abstract class EndlessRecyclerViewScrollListener(
         recyclerView.adapter?.let { adapter ->
             if (adapter.itemCount != 0) {
                 val lastVisibleItemPosition =
-                    layoutManager.findLastCompletelyVisibleItemPosition()
+                    (layoutManager as? GridLayoutManager)?.findLastCompletelyVisibleItemPosition()
                 if (lastVisibleItemPosition != RecyclerView.NO_POSITION
                     && lastVisibleItemPosition == adapter.itemCount - 1
                 ) return true
@@ -97,6 +112,18 @@ abstract class EndlessRecyclerViewScrollListener(
         return false
     }
 
+    private fun getLastVisibleItem(lastVisibleItemPositions: IntArray): Int {
+        var maxSize = 0
+        for (i in lastVisibleItemPositions.indices) {
+            if (i == 0) {
+                maxSize = lastVisibleItemPositions[i]
+            } else if (lastVisibleItemPositions[i] > maxSize) {
+                maxSize = lastVisibleItemPositions[i]
+            }
+        }
+        return maxSize
+    }
+
     // Defines the process for actually loading more data based on page
-    abstract fun onLoadMore(page: Int)
+    abstract fun onLoadMore(page: Int, take: Int, view: RecyclerView)
 }
--- networking/src/main/java/ghoudan/ayoub/networking/repository/MoviesRepositoryImpl.kt
+++ networking/src/main/java/ghoudan/ayoub/networking/repository/MoviesRepositoryImpl.kt
@@ -2,17 +2,18 @@ package ghoudan.ayoub.networking.repository
 
 import androidx.room.withTransaction
 import dagger.Reusable
+import ghoudan.ayoub.networking.offline.NetworkHandler
 import ghoudan.ayoub.local_models.models.Movies
 import ghoudan.ayoub.networking.api.Api
 import ghoudan.ayoub.networking.dao.MoviesDAO
 import ghoudan.ayoub.networking.database.AppDataBase
 import ghoudan.ayoub.networking.model.mapper.toAppModel
 import ghoudan.ayoub.networking.response.ResourceResponse
-import ghoudan.ayoub.networking.response.networkBoundResource
 import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onStart
@@ -22,6 +23,7 @@ class MoviesRepositoryImpl @Inject constructor(
     private val api: Api,
     private val moviesDao: MoviesDAO,
     private val db: AppDataBase,
+    private val networkHandler: NetworkHandler
 ) : MoviesRepository {
 
     override fun searchMovies(
@@ -29,17 +31,36 @@ class MoviesRepositoryImpl @Inject constructor(
         query: String
     ): Flow<ResourceResponse<List<Movies>>> {
         return flow<ResourceResponse<List<Movies>>> {
-            val result = api.searchMovies(1, query)
-            emit(ResourceResponse.Success(result.data.map { it.toAppModel() }))
+            if (networkHandler.checkNetworkAvailability()) {
+                val result = api.searchMovies(pageNumber, query)
+                emit(ResourceResponse.Success(result.data.map { it.toAppModel() }))
+            } else {
+                val data = moviesDao.searchMovies(pageNumber.times(30), query)
+                emit(ResourceResponse.Success(data))
+            }
         }.catch { exception ->
             emit(ResourceResponse.Error(exception))
         }.onStart { emit(ResourceResponse.Loading()) }
     }
 
-     fun fetchMovies2(pageNumber: Int): Flow<ResourceResponse<List<Movies>>> {
+    override fun fetchMovies(pageNumber: Int): Flow<ResourceResponse<List<Movies>>> {
         return flow<ResourceResponse<List<Movies>>> {
-            val result = api.getPopularMovies(pageNumber)
-            emit(ResourceResponse.Success(result.data.map { it.toAppModel() }))
+            if (networkHandler.checkNetworkAvailability()) {
+                val result = api.getPopularMovies(pageNumber)
+                db.withTransaction {
+                    moviesDao.insertMovies(result.data.map { it.toAppModel() })
+                }
+                emit(ResourceResponse.Success(result.data.map { it.toAppModel() }))
+            } else {
+                emit(ResourceResponse.Success(moviesDao.getMovies()))
+            }
+        }.catch { exception ->
+            emit(ResourceResponse.Error(exception))
+        }.onStart { emit(ResourceResponse.Loading()) }
+    }
+    override fun getFavoriteMovies(): Flow<ResourceResponse<List<Movies>>> {
+        return flow<ResourceResponse<List<Movies>>> {
+            emit(ResourceResponse.Success(moviesDao.getFavoriteMovies()))
         }.catch { exception ->
             emit(ResourceResponse.Error(exception))
         }.onStart { emit(ResourceResponse.Loading()) }
@@ -47,10 +68,15 @@ class MoviesRepositoryImpl @Inject constructor(
 
     override fun fetchMovieDetails(movieId: Int): Flow<ResourceResponse<Movies>> {
         return flow<ResourceResponse<Movies>> {
-            val result = api.getMovieDetails(movieId)
-            emit(
-                ResourceResponse.Success(result.toAppModel())
-            )
+            if (networkHandler.checkNetworkAvailability()) {
+                val result = api.getMovieDetails(movieId)
+                emit(
+                    ResourceResponse.Success(result.toAppModel())
+                )
+            } else {
+                val data = moviesDao.getMovie(movieId)
+                emit(ResourceResponse.Success(data))
+            }
         }.catch { exception ->
             emit(ResourceResponse.Error(exception))
         }.onStart { emit(ResourceResponse.Loading()) }
@@ -58,25 +84,10 @@ class MoviesRepositoryImpl @Inject constructor(
 
     override fun addMovieToFavorites(movie: Movies): Flow<Boolean> {
         return flow {
-            moviesDao.updateFavoriteMovieById(movie.id,movie.isFavorite)
+            moviesDao.updateFavoriteMovieById(movie.id, movie.isFavorite)
             emit(true)
         }.catch {
             emit(false)
         }.flowOn(Dispatchers.IO)
     }
-
-
-    override fun fetchMovies(pageNumber: Int) = networkBoundResource(
-        query = {
-            moviesDao.getMovies(pageNumber.times(10))
-        },
-        fetch = {
-            api.getPopularMovies(pageNumber).data.map { it.toAppModel() }
-        },
-        saveFetchResult = { movies ->
-            db.withTransaction {
-                moviesDao.insertMovies(movies)
-            }
-        }
-    )
 }
